0f7aeeab3929acfbaa35a8b9b46ceec7ac3572f8
bobbyworld88 <70490222+bobbyworld88@users.noreply.github.com>
2020-12-08 23:45:28 -0600

    week3updates

--- week3/array_lis/week3.js
+++ week3/array_lis/week3.js
@@ -1,13 +1,10 @@
-var helloWorld = document.createElement("h1");
-var 
-
 (function tenTimes() {
     for (var i = 0; i < 10; i++) {
         var h1 = document.createElement("h1");
         var content = document.createTextNode("Hello World");
         h1.appendChild(content);
         document.body.appendChild(h1);
-        document.getElementsByTagName("H1")[i].setAttribute("class", "democlass");
+        document.getElementsByTagName("h1")[i].setAttribute("class", "democlass");
       }
    })();
    
