6efeb38f24dd84565d22aba1e785282576c1f6c1
bobbyworld88 <70490222+bobbyworld88@users.noreply.github.com>
2020-12-09 01:29:59 -0600

    week7

---
+++ week7/toDo.js
@@ -0,0 +1,23 @@
+function submitForm() {
+    let nameInput = document.getElementById("name1").value;
+    let emailInput = document.getElementById("email1").value;
+    let messageInput = document.getElementById("message1").value;
+    alert(
+      "Name : " +
+        nameInput +
+        "\nE-mail : " +
+        emailInput +
+        "\nMessage : " +
+        messageInput +
+        ""
+    );
+    document.contact - form.submit();
+  }
+  
+  function clearText() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  }
+
+  
